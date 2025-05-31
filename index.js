a98cc3c79b0f38730e828a5bdbf7ec6fde87cf24
rwbgoachic <rwbchicago@gmail.com>
2025-05-31 02:23:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// @paysurity/common-utils
+// Placeholder entry point
+
+module.exports = {
+  logInfo: (msg) => {
+    console.log(`[INFO] ${msg}`);
+  },
+  logError: (msg) => {
+    console.error(`[ERROR] ${msg}`);
+  }
+};
